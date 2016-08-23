efe05a3b539a717a93d906b49f7c16d39e42cf80
Tanner Embry <awesomete@gmail.com>
2016-08-23 16:08:25 -0700

    Update to better json output + separate classes

--- src/main/java/com/tannerembry/metrics/JsonExporter.java
+++ src/main/java/com/tannerembry/metrics/JsonExporter.java
@@ -1,142 +1,58 @@
 package com.tannerembry.metrics;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.util.Base64;
-import com.google.api.client.util.store.FileDataStoreFactory;
-
-import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.*;
 import com.google.api.services.gmail.Gmail;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+/**
+ * This class gets an initialized mail instance and then exports the different json files.
+ */
 public class JsonExporter {
-    /** Application name. */
-    private static final String APPLICATION_NAME =
-            "Gmail API Java Quickstart";
 
-    /** Directory to store user credentials for this application. */
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-            System.getProperty("user.home"), ".credentials/gmail-java-quickstart");
-
-    /** Global instance of the {@link FileDataStoreFactory}. */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-            JacksonFactory.getDefaultInstance();
-
-    /** Global instance of the HTTP transport. */
-    private static HttpTransport HTTP_TRANSPORT;
-
-    /** Global instance of the scopes required by this quickstart.
-     *
-     * If modifying these scopes, delete your previously saved credentials
-     * at ~/.credentials/gmail-java-quickstart
-     */
-    private static final List<String> SCOPES =
-            Arrays.asList(GmailScopes.GMAIL_READONLY);
-
-    static {
-        try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(1);
-        }
-    }
-
-    /**
-     * Creates an authorized Credential object.
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize() throws IOException {
-        try {
-            // Load client secrets.
-            InputStream in =
-                    JsonExporter.class.getResourceAsStream("/client_secret.json");
-            GoogleClientSecrets clientSecrets =
-                    GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-            // Build flow and trigger user authorization request.
-            GoogleAuthorizationCodeFlow flow =
-                    new GoogleAuthorizationCodeFlow.Builder(
-                            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                            .setDataStoreFactory(DATA_STORE_FACTORY)
-                            .setAccessType("offline")
-                            .build();
-            Credential credential = new AuthorizationCodeInstalledApp(
-                    flow, new LocalServerReceiver()).authorize("user");
-            System.out.println(
-                    "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-            return credential;
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * Build and return an authorized Gmail client service.
-     * @return an authorized Gmail client service
-     * @throws IOException
-     */
-    public static Gmail getGmailService() throws IOException {
-        Credential credential = authorize();
-        return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
+    private static File exportDirectory;
 
     public static void main(String[] args) throws IOException {
+        try {
+            exportDirectory = new File(args[0]);
+            if(!exportDirectory.exists())
+                exportDirectory = new File(System.getProperty("user.dir"));
+        } catch(Exception e){
+            exportDirectory = new File(System.getProperty("user.dir"));
+        }
         // Build a new authorized API client service.
-        Gmail service = getGmailService();
+        Gmail service = MailInitializer.getGmailService();
 
         String query = "in:inbox from:(member@paypal.com) subject:(Notification of payment received)";
 
-        List<PaypalPurchase> allPurchases = getAllPurchases(service, "me", query);
+        List<PaypalPurchase> allPurchases = MailInitializer.getAllPurchases(service, "me", query);
 
-        exportToTimeseriesJson(allPurchases);
-        exportToDonutTimeOfDay(allPurchases);
-        exportToDayofWeekJson(allPurchases);
-        exportToEmailTypes(allPurchases);
+        exportMonthlyData(allPurchases);
+        exportClockData(allPurchases);
+        exportDayData(allPurchases);
+        exportEmailData(allPurchases);
     }
 
-    private static void exportToTimeseriesJson(List<PaypalPurchase> purchases) {
+    private static void exportMonthlyData(List<PaypalPurchase> purchases) {
         try {
-            File dir = new File(System.getProperty("user.dir"));
-            File jsonFile = new File(dir, "purchases_dates.json");
+            File jsonFile = new File(exportDirectory, "purchases_monthly.json");
             if (!jsonFile.exists())
                 jsonFile.createNewFile();
 
             //format JSON as shown in this example: http://c3js.org/samples/timeseries.html
 
             JSONObject json = new JSONObject();
-            json.put("x", "x");
-            JSONArray columns = new JSONArray();
 
             JSONArray dateColumn = new JSONArray();
             JSONArray shopColumn = new JSONArray();
-            shopColumn.add("Shop");
             JSONArray machinesColumn = new JSONArray();
-            machinesColumn.add("Machines");
             JSONArray arathiColumn = new JSONArray();
-            arathiColumn.add("Arathi Basin");
 
             //aggregate all purchases by month and count total
             HashMap<String, Integer> shopDownloads = new HashMap<String, Integer>();
@@ -186,25 +102,23 @@ public class JsonExporter {
                 if(shopDownloads.containsKey(date))
                     shopColumn.add(shopDownloads.get(date));
                 else
-                    shopColumn.add(0);
+                    shopColumn.add(null);
 
                 if(machinesDownloads.containsKey(date))
                     machinesColumn.add(machinesDownloads.get(date));
                 else
-                    machinesColumn.add(0);
+                    machinesColumn.add(null);
 
                 if(arathiDownloads.containsKey(date))
                     arathiColumn.add(arathiDownloads.get(date));
                 else
-                    arathiColumn.add(0);
+                    arathiColumn.add(null);
             }
 
-            dateColumn.add(0, "x");
-            columns.add(dateColumn);
-            columns.add(shopColumn);
-            columns.add(machinesColumn);
-            columns.add(arathiColumn);
-            json.put("columns", columns);
+            json.put("x", dateColumn);
+            json.put("Shop", shopColumn);
+            json.put("Machines", machinesColumn);
+            json.put("Arathi Basin", arathiColumn);
 
             FileWriter file = new FileWriter(jsonFile);
             file.write(json.toJSONString());
@@ -216,26 +130,20 @@ public class JsonExporter {
         }
     }
 
-    private static void exportToDonutTimeOfDay(List<PaypalPurchase> purchases) {
+    private static void exportClockData(List<PaypalPurchase> purchases) {
         try {
-            File dir = new File(System.getProperty("user.dir"));
-            File jsonFile = new File(dir, "purchases_times.json");
+            File jsonFile = new File(exportDirectory, "purchases_clock.json");
             if (!jsonFile.exists())
                 jsonFile.createNewFile();
 
             //format JSON as shown in this example: http://c3js.org/samples/timeseries.html
 
             JSONObject json = new JSONObject();
-            JSONArray columns = new JSONArray();
 
             JSONArray morningColumn = new JSONArray();
-            morningColumn.add("Morning");
             JSONArray afternoonColumn = new JSONArray();
-            afternoonColumn.add("Afternoon");
             JSONArray eveningColumn = new JSONArray();
-            eveningColumn.add("Evening");
             JSONArray nightColumn = new JSONArray();
-            nightColumn.add("Night");
 
             //aggregate all purchases by time of day purchased
             //Morning, Afternoon, Evening, Night
@@ -277,11 +185,10 @@ public class JsonExporter {
                 }
             }
 
-            columns.add(morningColumn);
-            columns.add(afternoonColumn);
-            columns.add(eveningColumn);
-            columns.add(nightColumn);
-            json.put("columns", columns);
+            json.put("Morning", morningColumn);
+            json.put("Afternoon", afternoonColumn);
+            json.put("Evening", eveningColumn);
+            json.put("Night", nightColumn);
 
             FileWriter file = new FileWriter(jsonFile);
             file.write(json.toJSONString());
@@ -293,28 +200,22 @@ public class JsonExporter {
         }
     }
 
-    private static void exportToDayofWeekJson(List<PaypalPurchase> purchases) {
+    private static void exportDayData(List<PaypalPurchase> purchases) {
         try {
-            File dir = new File(System.getProperty("user.dir"));
-            File jsonFile = new File(dir, "purchases_daysInWeek.json");
+            File jsonFile = new File(exportDirectory, "purchases_days.json");
             if (!jsonFile.exists())
                 jsonFile.createNewFile();
 
             //format JSON as shown in this example: http://c3js.org/samples/timeseries.html
 
             JSONObject json = new JSONObject();
-            json.put("x", "x");
-            JSONArray columns = new JSONArray();
 
             JSONArray dayColumn = new JSONArray();
             String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
             dayColumn.addAll(new ArrayList<String>(Arrays.asList(days)));
             JSONArray shopColumn = new JSONArray();
-            shopColumn.add("Shop");
             JSONArray machinesColumn = new JSONArray();
-            machinesColumn.add("Machines");
             JSONArray arathiColumn = new JSONArray();
-            arathiColumn.add("Arathi Basin");
 
             //aggregate all purchases by day and count total
             HashMap<String, Integer> shopDownloads = new HashMap<String, Integer>();
@@ -360,25 +261,23 @@ public class JsonExporter {
                 if(shopDownloads.containsKey(day))
                     shopColumn.add(shopDownloads.get(day));
                 else
-                    shopColumn.add(0);
+                    shopColumn.add(null);
 
                 if(machinesDownloads.containsKey(day))
                     machinesColumn.add(machinesDownloads.get(day));
                 else
-                    machinesColumn.add(0);
+                    machinesColumn.add(null);
 
                 if(arathiDownloads.containsKey(day))
                     arathiColumn.add(arathiDownloads.get(day));
                 else
-                    arathiColumn.add(0);
+                    arathiColumn.add(null);
             }
 
-            dayColumn.add(0, "x");
-            columns.add(dayColumn);
-            columns.add(shopColumn);
-            columns.add(machinesColumn);
-            columns.add(arathiColumn);
-            json.put("columns", columns);
+            json.put("x", dayColumn);
+            json.put("Shop", shopColumn);
+            json.put("Machines", machinesColumn);
+            json.put("Arathi Basin", arathiColumn);
 
             FileWriter file = new FileWriter(jsonFile);
             file.write(json.toJSONString());
@@ -400,17 +299,15 @@ public class JsonExporter {
         return null;
     }
 
-    private static void exportToEmailTypes(List<PaypalPurchase> purchases) {
+    private static void exportEmailData(List<PaypalPurchase> purchases) {
         try {
-            File dir = new File(System.getProperty("user.dir"));
-            File jsonFile = new File(dir, "purchases_emails.json");
+            File jsonFile = new File(exportDirectory, "purchases_emails.json");
             if (!jsonFile.exists())
                 jsonFile.createNewFile();
 
             //format JSON as shown in this example: http://c3js.org/samples/timeseries.html
 
             JSONObject json = new JSONObject();
-            JSONArray columns = new JSONArray();
 
             //aggregate all purchases by email service purchased on
             HashMap<String, Integer> emailDownloads = new HashMap<String, Integer>();
@@ -440,13 +337,11 @@ public class JsonExporter {
                     otherColumn.add(emailDownloads.get(key));
                 else {
                     JSONArray emailColumn = new JSONArray();
-                    emailColumn.add(key);
                     emailColumn.add(emailDownloads.get(key));
-                    columns.add(emailColumn);
+                    json.put(key, emailColumn);
                 }
             }
-            columns.add(otherColumn);
-            json.put("columns", columns);
+            json.put("other", otherColumn);
 
             FileWriter file = new FileWriter(jsonFile);
             file.write(json.toJSONString());
@@ -457,49 +352,4 @@ public class JsonExporter {
             e.printStackTrace();
         }
     }
-
-    /**
-     * List all Messages of the user's mailbox matching the query.
-     *
-     * @param service Authorized Gmail API instance.
-     * @param userId User's email address. The special value "me"
-     * can be used to indicate the authenticated user.
-     * @param query String used to filter the Messages listed.
-     * @throws IOException
-     */
-    public static List<PaypalPurchase> getAllPurchases(Gmail service, String userId,
-                                                          String query) throws IOException {
-        List<PaypalPurchase> allPurchases = new ArrayList<PaypalPurchase>();
-        ListMessagesResponse response = service.users().messages().list(userId).setQ(query).execute();
-
-        List<Message> messages = new ArrayList<Message>();
-        while (response.getMessages() != null) {
-            messages.addAll(response.getMessages());
-            if (response.getNextPageToken() != null) {
-                String pageToken = response.getNextPageToken();
-                response = service.users().messages().list(userId).setQ(query)
-                        .setPageToken(pageToken).execute();
-            } else {
-                break;
-            }
-        }
-
-        for(Message message : messages) {
-            Message m = service.users().messages().get(userId, message.getId()).execute();
-
-            //this message part is what contains all paypal information
-            MessagePart part = m.getPayload().getParts().get(0);
-
-            //need to decode the actual body of the message
-            byte[] decoded = Base64.decodeBase64(part.getBody().toString());
-            String actual = new String(decoded, "UTF8");
-
-            //use the decoded string and parse out all necessary data
-            PaypalPurchase purchase = PaypalPurchase.parse(actual);
-            allPurchases.add(purchase);
-        }
-
-        return allPurchases;
-    }
-
 }
---
+++ src/main/java/com/tannerembry/metrics/MailInitializer.java
@@ -0,0 +1,150 @@
+package com.tannerembry.metrics;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.Base64;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.*;
+import com.google.api.services.gmail.Gmail;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class MailInitializer {
+    /** Application name. */
+    private static final String APPLICATION_NAME =
+            "Gmail API Java Quickstart";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+            System.getProperty("user.home"), ".credentials/gmail-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+            JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/gmail-java-quickstart
+     */
+    private static final List<String> SCOPES =
+            Arrays.asList(GmailScopes.GMAIL_READONLY);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        try {
+            // Load client secrets.
+            InputStream in =
+                    MailInitializer.class.getResourceAsStream("/client_secret.json");
+            GoogleClientSecrets clientSecrets =
+                    GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+            // Build flow and trigger user authorization request.
+            GoogleAuthorizationCodeFlow flow =
+                    new GoogleAuthorizationCodeFlow.Builder(
+                            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                            .setDataStoreFactory(DATA_STORE_FACTORY)
+                            .setAccessType("offline")
+                            .build();
+            Credential credential = new AuthorizationCodeInstalledApp(
+                    flow, new LocalServerReceiver()).authorize("user");
+            System.out.println(
+                    "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+            return credential;
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Build and return an authorized Gmail client service.
+     * @return an authorized Gmail client service
+     * @throws IOException
+     */
+    public static Gmail getGmailService() throws IOException {
+        Credential credential = authorize();
+        return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    /**
+     * List all Messages of the user's mailbox matching the query.
+     *
+     * @param service Authorized Gmail API instance.
+     * @param userId User's email address. The special value "me"
+     * can be used to indicate the authenticated user.
+     * @param query String used to filter the Messages listed.
+     * @throws IOException
+     */
+    public static List<PaypalPurchase> getAllPurchases(Gmail service, String userId,
+                                                          String query) throws IOException {
+        List<PaypalPurchase> allPurchases = new ArrayList<PaypalPurchase>();
+        ListMessagesResponse response = service.users().messages().list(userId).setQ(query).execute();
+
+        List<Message> messages = new ArrayList<Message>();
+        while (response.getMessages() != null) {
+            messages.addAll(response.getMessages());
+            if (response.getNextPageToken() != null) {
+                String pageToken = response.getNextPageToken();
+                response = service.users().messages().list(userId).setQ(query)
+                        .setPageToken(pageToken).execute();
+            } else {
+                break;
+            }
+        }
+
+        for(Message message : messages) {
+            Message m = service.users().messages().get(userId, message.getId()).execute();
+
+            //this message part is what contains all paypal information
+            MessagePart part = m.getPayload().getParts().get(0);
+
+            //need to decode the actual body of the message
+            byte[] decoded = Base64.decodeBase64(part.getBody().toString());
+            String actual = new String(decoded, "UTF8");
+
+            //use the decoded string and parse out all necessary data
+            PaypalPurchase purchase = PaypalPurchase.parse(actual);
+            allPurchases.add(purchase);
+        }
+
+        return allPurchases;
+    }
+
+}
